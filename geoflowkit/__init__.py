f5701cc66187ee8758310d70b58d3073ba1b06bd
djw-easy <1303886435@qq.com>
2025-02-04 15:15:18 +0800

    use name gfk

--- geoflowkit/__init__.py
+++ geoflowkit/__init__.py
@@ -1,6 +1,6 @@
 from geoflowkit.flowdataframe import FlowDataFrame
-from geoflowkit.io import read_csv, read_file
-from geoflowkit.utils.spatial import second_order_density, k_func, l_func, local_l_func
 from geoflowkit.flowprocess import pairwise_distances
+from geoflowkit.io import read_csv, read_file, flows_from_od, flows_from_geometry
+from geoflowkit.utils.spatial import second_order_density, k_func, l_func, local_l_func
 
-from geoflowkit.utils.ftsne.ftsne import FTSNE
+from geoflowkit.utils.ftsne import FTSNE
