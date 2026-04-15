bc434d174a651c59a55e5d0ea9627fb4c908d2c4
djw-easy <1303886435@qq.com>
2026-04-15 13:57:35 +0800

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="geoflowkit",
-    version="0.1.7",
+    version="0.1.8",
     packages=find_packages(),
     install_requires=[
         "shapely",
