a28fb65b33a64a500ae471e442e9c0de253e5895
djw-easy <1303886435@qq.com>
2026-07-25 14:41:20 +0800

    bump version to 0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="geoflowkit",
-    version="0.3.0",
+    version="0.3.1",
     packages=find_packages(),
     install_requires=[
         "shapely",
